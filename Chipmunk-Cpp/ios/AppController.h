d0f554ce276dd770550111675458851e115ab867
vsilux <vsilux@gmail.com>
2012-12-12 12:31:52 +0200

    Initial Commit

---
+++ Chipmunk-Cpp/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Chipmunk_CppAppController.h
+//  Chipmunk-Cpp
+//
+//  Created by Artyom Peshkov on 12.12.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
